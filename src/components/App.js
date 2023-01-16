efb28c7e099eec94f6706f21d4e20603bbae4f24
pkdc <pkdchoy@gmail.com>
2023-01-16 17:18:58 +0000

    basic setup using apollo

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Card from './UI/Card';
+function App() {
+  return (
+    <Card>
+      <p> Hello</p>
+    </Card>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  url: "https://learn.01founders.co/api/graphql-engine/v1/graphql"
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+// different from tut
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
